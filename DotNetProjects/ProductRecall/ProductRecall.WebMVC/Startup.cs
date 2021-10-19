b6003f3858bceb9ef91e983a763826aef8c8264b
Samir Omerani <omegraph@gmail.com>
2021-10-19 19:44:19 -0400

    First Commit

---
+++ DotNetProjects/ProductRecall/ProductRecall.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProductRecall.WebMVC.Startup))]
+namespace ProductRecall.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
